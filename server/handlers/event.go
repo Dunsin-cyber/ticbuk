0aec9d780c635ab30331e1079e55373dee32e5d9
Dunsin <ayodejiabisuwa23@gmail.com>
2026-01-03 07:37:15 +0100

    feat: create event endpoints

--- server/handlers/event.go
+++ server/handlers/event.go
@@ -3,6 +3,7 @@ package handlers
 import (
 	"context"
 	"net/http"
+	"strconv"
 	"time"
 
 	"github.com/Dunsin-cyber/ticbuk/models"
@@ -20,7 +21,9 @@ func NewEventHandler(router *echo.Group, repository models.EventRepository) {
 
 	router.GET("", handler.GetMany)
 	router.POST("", handler.CreateOne)
-	router.GET(":eventId", handler.GetOne)
+	router.GET("/:eventId", handler.GetOne)
+	router.PUT("/:eventId", handler.UpdateOne)
+	router.DELETE("/:eventId", handler.DeleteOne)
 
 }
 
@@ -36,16 +39,106 @@ func (h *EventHandler) GetMany(ctx echo.Context) error {
 		})
 	}
 	return ctx.JSON(http.StatusOK, map[string]interface{}{
-		"status": "success",
+		"status":  "success",
 		"message": "events retrieved successfully",
-		"data":   events,
+		"data":    events,
 	})
 
 }
 
 func (h *EventHandler) GetOne(ctx echo.Context) error {
-	return nil
+	eventId, _ := strconv.Atoi(ctx.Param("eventId"))
+
+	context, cancel := context.WithTimeout(context.Background(), time.Duration(5*time.Second))
+	defer cancel()
+
+	event, err := h.repository.GetOne(context, uint(eventId))
+	if err != nil {
+		return ctx.JSON(http.StatusInternalServerError, map[string]interface{}{
+			"status":  "fail",
+			"message": err.Error(),
+		})
+	}
+	return ctx.JSON(http.StatusOK, map[string]interface{}{
+		"status":  "success",
+		"message": "event retrieved successfully",
+		"data":    event,
+	})
+
 }
 func (h *EventHandler) CreateOne(ctx echo.Context) error {
-	return nil
+	event := &models.Event{}
+	if err := ctx.Bind(event); err != nil {
+		return ctx.JSON(http.StatusBadRequest, map[string]interface{}{
+			"status":  "fail",
+			"message": err.Error(),
+			"data":    nil,
+		})
+	}
+
+	context, cancel := context.WithTimeout(context.Background(), time.Duration(5*time.Second))
+	defer cancel()
+
+	createdEvent, err := h.repository.CreateOne(context, event)
+	if err != nil {
+		return ctx.JSON(http.StatusInternalServerError, map[string]interface{}{
+			"status":  "fail",
+			"message": err.Error(),
+		})
+	}
+	return ctx.JSON(http.StatusCreated, map[string]interface{}{
+		"status":  "success",
+		"message": "event created successfully",
+		"data":    createdEvent,
+	})
+}
+
+func (h *EventHandler) UpdateOne(ctx echo.Context) error {
+	eventId, _ := strconv.Atoi(ctx.Param("eventId"))
+	updateData := make(map[string]interface{})
+
+	context, cancel := context.WithTimeout(context.Background(), time.Duration(5*time.Second))
+	defer cancel()
+
+	if err := ctx.Echo().JSONSerializer.Deserialize(ctx, &updateData); err != nil {
+		return ctx.JSON(http.StatusBadRequest, map[string]interface{}{
+			"status":  "fail",
+			"message": err.Error(),
+			"data":    nil,
+		})
+	}
+
+	event, err := h.repository.UpdateOne(context, uint(eventId), updateData)
+	if err != nil {
+		return ctx.JSON(http.StatusInternalServerError, map[string]interface{}{
+			"status":  "fail",
+			"message": err.Error(),
+		})
+	}
+	return ctx.JSON(http.StatusOK, map[string]interface{}{
+		"status":  "success",
+		"message": "event updated successfully",
+		"data":    event,
+	})
+
+}
+
+func (h *EventHandler) DeleteOne(ctx echo.Context) error {
+	eventId, _ := strconv.Atoi(ctx.Param("eventId"))
+
+	context, cancel := context.WithTimeout(context.Background(), time.Duration(5*time.Second))
+	defer cancel()
+
+	err := h.repository.DeleteOne(context, uint(eventId))
+	if err != nil {
+		return ctx.JSON(http.StatusInternalServerError, map[string]interface{}{
+			"status":  "fail",
+			"message": err.Error(),
+		})
+	}
+	return ctx.JSON(http.StatusOK, map[string]interface{}{
+		"status":  "success",
+		"message": "event deleted successfully",
+	})
+
 }
--- server/repositories/event.go
+++ server/repositories/event.go
@@ -20,7 +20,7 @@ func NewEventRepository(db *gorm.DB) models.EventRepository {
 func (er *EventRepository) GetMany(ctx context.Context) ([]*models.Event, error) {
 	events := []*models.Event{}
 
-	res := er.db.Model(&models.Event{}).WithContext(ctx).Find(&events)
+	res := er.db.Model(&models.Event{}).WithContext(ctx).Order("updated_at desc").Find(&events)
 
 	if res.Error != nil {
 		return nil, res.Error
@@ -28,10 +28,44 @@ func (er *EventRepository) GetMany(ctx context.Context) ([]*models.Event, error)
 	return events, nil
 }
 
-func (er *EventRepository) GetOne(ctx context.Context, eventId string) (*models.Event, error) {
-	return nil, nil
+func (er *EventRepository) GetOne(ctx context.Context, eventId uint) (*models.Event, error) {
+	event := &models.Event{}
+
+	res := er.db.Model(event).WithContext(ctx).Where("id = ?", eventId).First(event)
+
+	if res.Error != nil {
+		return nil, res.Error
+	}
+	return event, nil
 }
 
-func (er *EventRepository) CreateOne(ctx context.Context, event models.Event) (*models.Event, error) {
-	return nil, nil
+func (er *EventRepository) CreateOne(ctx context.Context, event *models.Event) (*models.Event, error) {
+	res := er.db.Model(event).WithContext(ctx).Create(event)
+
+	if res.Error != nil {
+		return nil, res.Error
+	}
+	return event, nil
+}
+
+func (er *EventRepository) UpdateOne(ctx context.Context, eventId uint, updateData map[string]interface{}) (*models.Event, error) {
+	event := &models.Event{}
+
+	updateRes := er.db.Model(event).Where("id = ?", eventId).Updates(updateData)
+
+	if updateRes.Error != nil {
+		return nil, updateRes.Error
+	}
+
+	getRes := er.db.Model(event).Where("id = ?", eventId).First(event)
+
+	if getRes.Error != nil {
+		return nil, getRes.Error
+	}
+	return event, nil
+}
+
+func (er *EventRepository) DeleteOne(ctx context.Context, eventId uint) error {
+	res := er.db.Model(&models.Event{}).WithContext(ctx).Delete(&models.Event{}, eventId)
+	return res.Error
 }
